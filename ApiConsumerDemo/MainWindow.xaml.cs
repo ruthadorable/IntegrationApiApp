77a2a7a7aa6b37288b42da2db9b550f6f3afab74
ruthadorable <ruthadorable@live.com>
2020-09-01 03:33:41 +0200

    Add files via upload

---
+++ ApiConsumerDemo/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using static DemoLibrary.ComicModel;
+using static DemoLibrary.ComicProcessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DemoLibrary;
+using WPFApp;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+ 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+            nextImageButton.IsEnabled = false;
+        }
+
+        
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+            if(imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+
+            }
+            
+            currentNumber = comic.Num;
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+
+            comicImage.Source = new BitmapImage(uriSource);
+
+
+        }
+        private async void Window_Loaded(object sender,RoutedEventArgs e)
+        {
+            await LoadImage();
+        }
+
+        private async void previousButton_Click(object sender,RoutedEventArgs e)
+        { if( currentNumber > 1)
+            {
+                currentNumber -= 1;
+                nextImageButton.IsEnabled = true;
+                await LoadImage(currentNumber);
+                if( currentNumber == 1 )
+                {
+                    previousImageButton.IsEnabled = false;
+                }
+            }
+        }
+
+        private async void sunInformation_Click(object sender, RoutedEventArgs e)
+        {
+            SunInfo sunInfo = new SunInfo();
+            sunInfo.Show();
+
+
+        }
+
+        private async void  nextButton_Click(object sender, RoutedEventArgs e)
+        { if(currentNumber < maxNumber)
+            {
+                currentNumber += 1;
+                previousImageButton.IsEnabled = true;
+                await LoadImage(currentNumber);
+                if(currentNumber==maxNumber)
+                {
+                    nextImageButton.IsEnabled = false;
+                }
+
+            }
+
+        }
+    }
+}
